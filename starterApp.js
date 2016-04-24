f0deb99ad64b54806208d6d279724d5c9205b055
an.sokolov <sokolik_85@list.ru>
2016-04-24 21:11:38 +0600

    add tabpanel with couple of pages

--- starterApp.js
+++ starterApp.js
@@ -1,18 +1,60 @@
 Ext.application({
     name:'app',
     requires : ['Ext.window.MessageBox', 'app.extJsBook'],
-    launch: function  () {
+    launch: function () {
 
         var book = Ext.create('app.extJsBook');
 
-        Ext.create('Ext.container.Viewport',{
+        // Ext.create('Ext.container.Viewport',{
+        //     layout:'fit',
+        //     items:[{
+        //         xtype:'container',
+        //         // html: 'A panel inside viewport',
+        //         defaults:{
+        //             margin:'5,5,0,5'
+        //         },
+        //         layout:'vbox',
+        //         items:[{
+        //             xtype:'textfield',
+        //             fieldLabel:'Name',
+        //             style:{marginBottom:'20px',color:'blue'}
+        //         },{
+        //             xtype:'button',
+        //             text:'submit'
+        //         },
+        //             {
+        //                 xtype:'buttongroup',
+        //                 colomns:3,
+        //                 title:'Days',
+        //                 items:[
+        //                     {xtype:'button', text:1},
+        //                     {xtype:'button', text:2},
+        //                     {xtype:'button', text:3}]
+        //             }
+        //         ]
+        //
+        //     }]
+        // });
+
+        Ext.create("Ext.tab.Panel",{
+         //   width:400,
+         //   height:400,
+            activeTab:0,
             layout:'fit',
-            items:[{
-                xtype:'panel',
-                width:300,
-                html:'A Panel inside viewport',
-                title:'Viewport Panel'
-            }]
+            //layout:'ablolute',
+            //x:10, y:10,
+            renderTo: document.body,
+            items:[
+                {
+                    title:'Page 1',
+                    html:'Page1'
+                },
+                {
+                    xtype:'panel',
+                    title:'Page 2',
+                    html:'Page2'
+                }
+            ]
         });
 
         // var extjsBookPanel = Ext.create('Ext.panel.Panel',{
