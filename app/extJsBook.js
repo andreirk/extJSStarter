6971e0cedb8cafbeacf7d7e984ad8756ee786bbf
an.sokolov <sokolik_85@list.ru>
2016-04-24 20:45:31 +0600

    add a viewPort and panel within it

--- app/extJsBook.js
+++ app/extJsBook.js
@@ -1,18 +1,22 @@
 Ext.define('app.extJsBook', {
-    config:{
-        title:'',
-        price:'',
-        author:''
-    },
+    xtype:'extjsButton',
+    extended:'Ext.button.Button',
+    // config:{
+    //     title:'',
+    //     price:'',
+    //     author:''
+    // },
     constructor:function  (config) {
-        this.initConfig(config);
-    },
-
-    applyTitle: function  (value) {
-        return Ext.String.capitalize(value.toUpperCase());
-    },
-
-    updateAuthor: function  (newValue, oldValue) {
-        console.log("NewVal - " + newValue + " : " + "Old Value - " + oldValue);
+        this.callParent(arguments);
+   //     this.initConfig(config);
     }
+    // ,
+
+    // applyTitle: function  (value) {
+    //     return Ext.String.capitalize(value.toUpperCase());
+    // },
+    //
+    // updateAuthor: function  (newValue, oldValue) {
+    //     console.log("NewVal - " + newValue + " : " + "Old Value - " + oldValue);
+    // }
 });
--- starterApp.js
+++ starterApp.js
@@ -2,25 +2,56 @@ Ext.application({
     name:'app',
     requires : ['Ext.window.MessageBox', 'app.extJsBook'],
     launch: function  () {
-        console.log("Welcome to Ext Starter Tutorial");
-        Ext.Msg.alert("Message", "Welcome to Ext Starter Tutorial");
 
         var book = Ext.create('app.extJsBook');
-        book.setTitle("ExtJSStarter");
-        book.setPrice('10$');
-        book.setAuthor('John');
 
-        console.log('Message 1 -', book.getTitle() );
-
-        Ext.Msg.alert('Message 1 - ', book.getTitle());
-
-        var book2 = Ext.create('app.extJsBook', {
-            title:"Ext JS Upgrade",
-            author:'Alex',
-            price:'20$',
+        Ext.create('Ext.container.Viewport',{
+            layout:'fit',
+            items:[{
+                xtype:'panel',
+                width:300,
+                html:'A Panel inside viewport',
+                title:'Viewport Panel'
+            }]
         });
 
-        Ext.Msg.alert("Message 2 - ", book2.getTitle());
+        // var extjsBookPanel = Ext.create('Ext.panel.Panel',{
+        //     bodyPadding: 5,
+        //     width : 300,
+        //     title: 'extjsBookPanel',
+        //     items:[{
+        //         xtype:'textfield',
+        //         fieldLabel: 'Publish date'
+        //     }
+        //          ,
+        //         // Ext.create('app.extjsButton', {
+        //         //     text: 'Custom button instantiated using Ext.create()'
+        //         // })
+        //         // ,
+        //         {
+        //             xtype: 'extjsButton',
+        //             text: 'Custom button instantiated using xtype'
+        //         }
+        //     ],
+        //     renderTo: Ext.getBody()
+        // });
+
+
+        // book.setTitle("ExtJSStarter");
+        // book.setPrice('10$');
+        // book.setAuthor('John');
+        //
+        // console.log('Message 1 -', book.getTitle() );
+        //
+        // Ext.Msg.alert('Message 1 - ', book.getTitle());
+        //
+        // var book2 = Ext.create('app.extJsBook', {
+        //     title:"Ext JS Upgrade",
+        //     author:'Alex',
+        //     price:'20$',
+        // });
+        //
+        // Ext.Msg.alert("Message 2 - ", book2.getTitle());
     }
 });
 
