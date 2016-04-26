d9dc446ed043ec0a475cce537977de89ca5bcf69
an.sokolov <sokolik_85@list.ru>
2016-04-26 21:03:48 +0600

    start new branch, to follow metanit tutorial

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by acer on 4/26/2016.
+ */
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    title: 'Приложение на Ext JS 4',
+                    html : '<h3>Добро пожаловать в мир Ext JS 4!</h3>'
+                }
+            ]
+        });
+    }
+});
